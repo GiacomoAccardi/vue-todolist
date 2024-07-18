f6bc982cd2bb3937d0ced1b8710982732f9798ac
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-18 15:52:39 +0200

    [UPD] There are some instruction into js file

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+1.Creare il layout della pagina
+2.stilizzare la pagina e il suo contenuto statico
+3.Creare un array di oggetti che contenga i "to do"
+4.sostituire con gli elementi dell'array il contenuto statico nel DOM
+5.in base al valore dello stato di ogni item nell'array l'elemento si posizionerà sotto la sezione "todo" o sotto la sezione "done"
+6.creare una funzione che permetta di rimuovere un "todo"
+7.Creare un campo di input che permetta l'inserimento di un "todo"
+*/
